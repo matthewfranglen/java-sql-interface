78188ae703ccc853112a2939e1d2256a2b9a63a5
Matthew Franglen <matthew@franglen.org>
2015-09-30 22:59:06 +0100

    Remove spring boot

--- generator/src/test/java/com/matthew/sql/generator/CodeGeneratorSteps.java
+++ generator/src/test/java/com/matthew/sql/generator/CodeGeneratorSteps.java
@@ -5,35 +5,20 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.util.Arrays;
 
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
-
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
-import com.matthew.sql.parser.SqlParser;
 import com.matthew.sql.parser.SqlStatement;
-import com.matthew.sql.parser.SqlStatementLoader;
 
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class CodeGeneratorSteps {
 
-    @Spy private SqlParser parser;
-    @InjectMocks private SqlStatementLoader loader;
-
     private String name;
     private String path;
     private String statement;
     private String code;
-
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
 
     @Given("^the name \"(.*)\"$")
     public void givenTheName(String name) throws IOException {
