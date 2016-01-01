e733d3090135baa1579d062d91ba6c0d62454bd0
Matthew Franglen <matthew@franglen.org>
2016-01-01 20:02:46 +0000

    Switch to walking the completed tree
    The order of events when actively parsing the tree can differ from the
    order when walking a complete tree. This can include not issuing enter
    events _at all_:
    
    http://www.antlr.org/api/Java/org/antlr/v4/runtime/Parser.html#addParseListener%28org.antlr.v4.runtime.tree.ParseTreeListener%29

--- parser/src/main/java/com/matthew/sql/parser/SqlParser.java
+++ parser/src/main/java/com/matthew/sql/parser/SqlParser.java
@@ -3,6 +3,7 @@ package com.matthew.sql.parser;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import com.matthew.sql.parser.generated.StatementLexer;
 import com.matthew.sql.parser.generated.StatementParser;
@@ -13,11 +14,14 @@ public class SqlParser {
     public SqlStatementBuilder parse(String content) {
         StatementLexer lexer = new StatementLexer(new ANTLRInputStream(content));
         StatementParser parser = new StatementParser(new CommonTokenStream(lexer));
-        StatementBuildingListener listener = new StatementBuildingListener();
 
         parser.setErrorHandler(new BailErrorStrategy());
-        parser.addParseListener(listener);
-        parser.root();
+        StatementParser.RootContext ctx = parser.root();
+
+        StatementBuildingListener listener = new StatementBuildingListener();
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+        walker.walk(listener, ctx);
 
         return listener.getBuilder();
     }
