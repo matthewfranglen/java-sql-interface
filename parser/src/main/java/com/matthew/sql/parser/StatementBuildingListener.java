3b0ab0dbfc7c602b28a6c15363e248ccff53a69e
Matthew Franglen <matthew@franglen.org>
2016-01-01 15:35:10 +0000

    Start collecting arguments

--- parser/src/main/java/com/matthew/sql/parser/StatementBuildingListener.java
+++ parser/src/main/java/com/matthew/sql/parser/StatementBuildingListener.java
@@ -2,6 +2,8 @@ package com.matthew.sql.parser;
 
 import com.matthew.sql.parser.generated.StatementBaseListener;
 import com.matthew.sql.parser.generated.StatementParser;
+import com.matthew.sql.statement.Argument;
+import com.matthew.sql.statement.ArgumentType;
 import com.matthew.sql.statement.SqlStatementBuilder;
 
 public class StatementBuildingListener extends StatementBaseListener {
@@ -14,6 +16,11 @@ public class StatementBuildingListener extends StatementBaseListener {
 
     public SqlStatementBuilder getBuilder() {
         return builder;
+    }
+
+    @Override
+    public void exitStatement(StatementParser.StatementContext ctx) {
+        builder.setStatement(ctx.getText());
     }
 
 	@Override
@@ -38,11 +45,27 @@ public class StatementBuildingListener extends StatementBaseListener {
 
 	@Override
     public void enterArgument(StatementParser.ArgumentContext ctx) {
+        ArgumentType type = getArgumentType(ctx.argument_type());
+        String name = ctx.argument_name().getText();
+
+        builder.addArgument(new Argument(type, name));
     }
 
-    @Override
-    public void exitStatement(StatementParser.StatementContext ctx) {
-        builder.setStatement(ctx.getText());
+    private ArgumentType getArgumentType(StatementParser.Argument_typeContext ctx) {
+        if (ctx.TEXT_TYPE() != null) {
+            return ArgumentType.TEXT;
+        }
+        if (ctx.WHOLE_NUMBER_TYPE() != null) {
+            return ArgumentType.WHOLE_NUMBER;
+        }
+        if (ctx.FRACTIONAL_NUMBER_TYPE() != null) {
+            return ArgumentType.FRACTIONAL_NUMBER;
+        }
+        if (ctx.TIMESTAMP_TYPE() != null) {
+            return ArgumentType.TIMESTAMP;
+        }
+
+        throw new RuntimeException("Unrecognized Argument Type: " + ctx.getText());
     }
 
 }
