079e8e361e5c8e6b6fdc69cd4079cea0708bea10
Matthew Franglen <matthew@franglen.org>
2016-01-01 15:23:46 +0000

    Track takes and returns lists
    Doesn't yet convert and collect arguments.

--- parser/src/main/java/com/matthew/sql/parser/StatementBuildingListener.java
+++ parser/src/main/java/com/matthew/sql/parser/StatementBuildingListener.java
@@ -16,6 +16,25 @@ public class StatementBuildingListener extends StatementBaseListener {
         return builder;
     }
 
+	@Override public void enterTakes_line(StatementParser.Takes_lineContext ctx) {
+        builder.enterTakes();
+    }
+
+	@Override public void exitTakes_line(StatementParser.Takes_lineContext ctx) {
+        builder.exitTakes();
+    }
+
+	@Override public void enterReturns_line(StatementParser.Returns_lineContext ctx) {
+        builder.enterReturns();
+    }
+
+	@Override public void exitReturns_line(StatementParser.Returns_lineContext ctx) {
+        builder.exitReturns();
+    }
+
+	@Override public void enterArgument(StatementParser.ArgumentContext ctx) {
+    }
+
     @Override
     public void exitStatement(StatementParser.StatementContext ctx) {
         builder.setStatement(ctx.getText());
--- parser/src/main/java/com/matthew/sql/statement/SqlStatementBuilder.java
+++ parser/src/main/java/com/matthew/sql/statement/SqlStatementBuilder.java
@@ -1,10 +1,27 @@
 package com.matthew.sql.statement;
 
+import static com.google.common.base.Preconditions.checkState;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Set;
+
 public class SqlStatementBuilder {
+
+    private static final Set<CollectionState> COLLECTING_STATES = EnumSet.of(
+            CollectionState.COLLECTING_TAKES, CollectionState.COLLECTING_RETURNS
+        );
 
     private String name;
     private String path;
     private String statement;
+
+    private Collection<Argument> takes;
+    private Collection<Argument> returns;
+
+    private CollectionState state;
+    private Collection<Argument> current;
 
     public void setName(String name) {
         this.name = name;
@@ -18,8 +35,52 @@ public class SqlStatementBuilder {
         this.statement = statement;
     }
 
+    public void enterTakes() {
+        checkState(takes == null, "Encountered two takes definitions");
+        checkState(state == CollectionState.NOT_COLLECTING, "Encountered takes definition inside argument list");
+
+        state = CollectionState.COLLECTING_TAKES;
+        current = new ArrayList<>();
+    }
+
+    public void exitTakes() {
+        checkState(state == CollectionState.COLLECTING_TAKES, "Left takes definition without entering it");
+
+        state = CollectionState.NOT_COLLECTING;
+        takes = current;
+        current = null;
+    }
+
+    public void enterReturns() {
+        checkState(returns == null, "Encountered two returns definitions");
+        checkState(state == CollectionState.NOT_COLLECTING, "Encountered takes definition inside argument list");
+
+        state = CollectionState.COLLECTING_RETURNS;
+        current = new ArrayList<>();
+    }
+
+    public void exitReturns() {
+        checkState(state == CollectionState.COLLECTING_RETURNS, "Left returns definition without entering it");
+
+        state = CollectionState.NOT_COLLECTING;
+        returns = current;
+        current = null;
+    }
+
+    public void addArgument(Argument argument) {
+        checkState(COLLECTING_STATES.contains(state), "Encountered argument outside of collection");
+
+        current.add(argument);
+    }
+
     public SqlStatement build() {
         return new SqlStatement(name, path, statement);
+    }
+
+    private enum CollectionState {
+        NOT_COLLECTING,
+        COLLECTING_TAKES,
+        COLLECTING_RETURNS;
     }
 
 }
