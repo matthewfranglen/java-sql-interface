b4e5f8f465b53015b4e6b61f905eb7e3ddf3534a
Matthew Franglen <matthew@franglen.org>
2016-01-02 22:03:00 +0000

    Accept takes and returns lists in tests

--- generator/src/test/java/com/matthew/sql/generator/CodeGeneratorSteps.java
+++ generator/src/test/java/com/matthew/sql/generator/CodeGeneratorSteps.java
@@ -3,10 +3,14 @@ package com.matthew.sql.generator;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
+import com.matthew.sql.statement.Argument;
+import com.matthew.sql.statement.ArgumentType;
 import com.matthew.sql.statement.SqlStatement;
 
 import cucumber.api.java.en.Given;
@@ -19,6 +23,8 @@ public class CodeGeneratorSteps {
     private String path;
     private String statement;
     private String code;
+    private List<Argument> takes;
+    private List<Argument> returns;
 
     @Given("^the name \"(.*)\"$")
     public void givenTheName(String name) throws IOException {
@@ -35,15 +41,40 @@ public class CodeGeneratorSteps {
         statement = readResource(file);
     }
 
+    @Given("^the takes \"(.*)\"$")
+    public void givenTheTakes(List<String> takes) {
+        this.takes = parseArgumentList(takes);
+    }
+
+    @Given("^the returns \"(.*)\"$")
+    public void givenTheReturns(List<String> returns) {
+        this.returns = parseArgumentList(returns);
+    }
+
+    private List<Argument> parseArgumentList(List<String> arguments) {
+        List<Argument> result = new ArrayList<>();
+
+        for (String argument : arguments) {
+            result.add(parseArgument(argument));
+        }
+
+        return result;
+    }
+
+    private Argument parseArgument(String argument) {
+        String[] split = argument.trim().split(" +", 2);
+        return new Argument(ArgumentType.valueOf(split[0].trim()), split[1].trim());
+    }
+
     @When("^the statement is converted to a statement handler class$")
     public void whenTheStatementIsConvertedToAStatementHandlerClass() throws Exception {
-        SqlStatement statementObject = new SqlStatement(name, path, statement, null, null);
+        SqlStatement statementObject = new SqlStatement(name, path, statement, takes, returns);
         code = new CodeGenerator().generateStatementHandlerCode(Arrays.asList(statementObject));
     }
 
     @When("^the statement is converted to a statement class$")
     public void whenTheStatementIsConvertedToAStatementClass() throws Exception {
-        SqlStatement statementObject = new SqlStatement(name, path, statement, null, null);
+        SqlStatement statementObject = new SqlStatement(name, path, statement, takes, returns);
         code = new CodeGenerator().generateStatementCode(statementObject);
     }
 
